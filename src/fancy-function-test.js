384d3d70cf365b3d3679a772fbe8f42039711ee0
chrisnienart <chris.nienart@minsa.io>
2024-11-17 18:59:22 -0500

    deleted boilerplate files

--- src/fancy-function-test.js
+++
@@ -1,8 +0,0 @@
-import { followedBySemi, generator } from "./fancy-function-import";
-
-const plugin = {
-  insertText: async function(app) {
-    followedBySemi({ "mymymy": "ohmy" }, [ "cat" ]);
-    generator("jingle");
-  }
-}
